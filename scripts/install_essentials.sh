47d4cf6e9bcb64bf66296399026d42c14ce3465f
hjmn6m <wiktor.bozek@aptiv.com>
2022-04-10 13:36:03 +0200

    run install esseintails as executable in CI

--- scripts/install_essentials.sh
+++ scripts/install_essentials.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
-function install_essentials() {
-    apt-get update && apt-get install -y \
-        build-essential \
-        wget \
-        git \
-        gcc \
-        clang \
-        g++ \
-        libegl1-mesa \
-        libgl1-mesa-dev \
-        libglu1-mesa \
-        libglu1-mesa-dev \
-        libsfml-dev \
-        libgtest-dev \
-        binutils-dev \
-        libtool \
-        lua5.2 \
-        liblua5.2-dev \
-        liblua5.2-0
-
-}
+apt-get update && apt-get install -y \
+    build-essential \
+    wget \
+    git \
+    gcc \
+    clang \
+    g++ \
+    libegl1-mesa \
+    libgl1-mesa-dev \
+    libglu1-mesa \
+    libglu1-mesa-dev \
+    libsfml-dev \
+    libgtest-dev \
+    binutils-dev \
+    libtool \
+    lua5.2 \
+    liblua5.2-dev \
+    liblua5.2-0
