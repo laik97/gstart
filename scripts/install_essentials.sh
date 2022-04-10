22da750486cf0ecb7f3ae448ce6690463dbab713
hjmn6m <wiktor.bozek@aptiv.com>
2022-04-10 13:46:10 +0200

    add sfml required packages to install stage

--- scripts/install_essentials.sh
+++ scripts/install_essentials.sh
@@ -12,22 +12,13 @@ sudo apt-get update && sudo apt-get install -y \
     libglu1-mesa \
     libglu1-mesa-dev \
     libsfml-dev \
+    libsfml-network2.5 \
+    libsfml-graphics2.5 \
+    libsfml-window2.5 \
+    libsfml-system2.5 \
     libgtest-dev \
     binutils-dev \
     libtool \
     lua5.2 \
     liblua5.2-dev \
     liblua5.2-0
-
-# apt install -y \
-#     libegl1-mesa \
-#     libgl1-mesa-dev \
-#     libglu1-mesa \
-#     libglu1-mesa-dev \
-#     libsfml-dev \
-#     libgtest-dev \
-#     binutils-dev \
-#     libtool \
-#     lua5.2 \
-#     liblua5.2-dev \
-#     liblua5.2-0
